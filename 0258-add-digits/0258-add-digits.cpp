b55de606538c6fe7ca7e652da3efb8be4d289d2f
GURKIRAT SINGH <work.gurkiratsingh@gmail.com>
2026-07-14 22:38:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.21%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(to_string(num).length()>1){
-            int n=0,len=to_string(num).length();
-            string s=to_string(num);
-            for (char c : s)
-                n += c - '0';
-            num=n;
-        }
-        return num;
+       if (num == 0)
+    return 0;
+
+if (num % 9 == 0)
+    return 9;
+
+return num % 9;
     }
 };
